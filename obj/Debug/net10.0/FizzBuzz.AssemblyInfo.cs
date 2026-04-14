34fa2bfe4d5d31e30c38d068551d90227ca0dd02
Lostigen <jimmy.lostigen@outlook.com>
2026-04-14 19:24:54 +0200

    System repair

--- obj/Debug/net10.0/FizzBuzz.AssemblyInfo.cs
+++ obj/Debug/net10.0/FizzBuzz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+befcb4637964183f0bc3c1adf9851ba478767b79")]
 [assembly: System.Reflection.AssemblyProductAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
