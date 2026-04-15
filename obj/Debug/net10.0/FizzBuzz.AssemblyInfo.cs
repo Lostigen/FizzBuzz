222179a7415242dffaf51d75fa37efa7985e925d
Lostigen <jimmy.lostigen@outlook.com>
2026-04-15 09:18:52 +0200

    main

--- obj/Debug/net10.0/FizzBuzz.AssemblyInfo.cs
+++ obj/Debug/net10.0/FizzBuzz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a39f6fc1bb8cfd497303791d5b219468a103e4c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec7a3371cf48b34003e3fa7ca5707b4e25c10782")]
 [assembly: System.Reflection.AssemblyProductAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
